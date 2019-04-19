f25263700eb9775ff745770658adb9e4235fc098
AM77 <amine.griche77@gmail.com>
2019-04-19 20:26:33 +0200

    created the program and tesed it.

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int caesar_key = 1;
+    char plain_text[191];
+    char encrypted_text[191];
+
+    printf("********* Caesar Cipher C Program ********* \n\n");
+
+    printf("[!] Please enter a text: ");
+
+    fgets(plain_text, sizeof(plain_text), stdin);
+
+    int text_length = strlen(plain_text) - 1;
+
+    printf("[+] The encrypted text is: ");
+
+    for(int i=0; i < text_length; i++){
+        printf("%c", (char)((int)plain_text[i] - 97 + caesar_key) % 26 + 97);
+    }
+
+    printf("\n");
     return 0;
 }
